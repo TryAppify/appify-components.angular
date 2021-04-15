fc0e82e7bd764ada25a5d9f9eee1f355cef868ba
Daniel Vásquez <53444923+Danvaspri@users.noreply.github.com>
2021-04-15 08:21:29 -0500

    Grid (#10)
    * Grid

--- components/appify-grid/appify-grid.component.spec.ts
+++ components/appify-grid/appify-grid.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AppifyGridComponent } from './appify-grid.component';
+import { AppifyGridComponent } from "./appify-grid.component";
 
-describe('AppifyGridComponent', () => {
-  let component: AppifyGridComponent;
-  let fixture: ComponentFixture<AppifyGridComponent>;
+describe("AppifyGridComponent", () => {
+    let component: AppifyGridComponent;
+    let fixture: ComponentFixture<AppifyGridComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppifyGridComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppifyGridComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppifyGridComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppifyGridComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
